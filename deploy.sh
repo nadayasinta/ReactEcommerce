94382e01aecf70d8c5b5f8237e94db005ab4d07f
nadayasinta <nada@alterra.id>
2019-08-28 14:38:25 +0700

    travis9

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
-docker stop reactecommerce
-docker rm reactecommerce
-docker rmi nadayasinta/reactecommerce:latest
-docker run -d --name reactecommerce -p 3000:80 nadayasinta/reactecommerce:latest
+sudo docker stop reactecommerce
+sudo docker rm reactecommerce
+sudo docker rmi nadayasinta/reactecommerce:latest
+sudo docker run -d --name reactecommerce -p 3000:80 nadayasinta/reactecommerce:latest
